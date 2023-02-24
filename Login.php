5b9b561b97cc1e65c6c2c7ab79f46418b26be088
kfngivkdvisdvmdkvn <106468984+kfngivkdvisdvmdkvn@users.noreply.github.com>
2023-02-24 23:22:56 +0700

    Update Login.php

--- Login.php
+++ Login.php
@@ -5,7 +5,7 @@ function ExitAlert($msg){
 
 $JDecode = json_decode(file_get_contents('php://input'),true); 
 $FileName = "LoginData.data";
-$ScriptName = "Script_Free_Edit_Obb35_v01.lua";
+$ScriptName = "555";
 $username= $JDecode["Username"];
 $password=  $JDecode["Password"];
 $content =json_decode(file_get_contents($FileName),true);
